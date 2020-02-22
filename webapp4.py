14008ffaf780d0b14e1cb9e37af7b85451dcc1de
David Beck <dbeck@ualberta.ca>
2020-02-22 14:41:41 -0700

    updated to GPL and added notifications to program files

--- webapp4.py
+++ webapp4.py
@@ -1,3 +1,27 @@
+
+'''
+******************************************************************
+SLEXIL—Software Linking Elan XML to Illuminated Language
+Copyright (C) 2019 Paul Shannon and David Beck
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+The full version of the GNU General Public License is found at
+<https://www.gnu.org/licenses/>.
+
+Information about the software can be obtained by contacting
+david.beck at ualberta.ca.
+******************************************************************
+'''
+
 import base64
 from zipfile import ZipFile
 
@@ -929,10 +953,10 @@ def createZipFile(projectDir, projectTitle):
 
 # ----------------------------------------------------------------------------------------------------
 # enable these lines for running from bash and python
-if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=60041)
+# if __name__ == "__main__":
+#     app.run_server(host='0.0.0.0', port=60041)
 
 # enable these lines if running with gunicorn
-# if __name__ == "__main__":
-#     server = app.server
-#     app.run()
+if __name__ == "__main__":
+    server = app.server
+    app.run()
