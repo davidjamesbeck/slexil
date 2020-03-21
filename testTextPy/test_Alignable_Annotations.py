fff48b1f4fc91bbf274239f71355247e4c12ed68
David Beck <David@iMac-2.local>
2020-03-21 12:56:05 -0600

    added new tests to master branch

--- testTextPy/test_Alignable_Annotations.py
+++ testTextPy/test_Alignable_Annotations.py
@@ -12,11 +12,172 @@ from ijalLine import IjalLine as Line
 
 # ----------------------------------------------------------------------------------------------------
 pd.set_option('display.width', 1000)
-
+pd.set_option('display.max_columns', 10)
 
 # ----------------------------------------------------------------------------------------------------
 def runTests(display=False):
-    test_Aktzini_plus_one(True)
+    # test_Aktzini_plus_one(True)
+    # test_HMDL_TimeCodes()
+    # test_aktzini_TimeCodes()
+    # test_Ghost_TimeCodes()
+    # test_inferno_TimeCodes()
+    # test_Jagpossum_TimeCodes()
+    # test_inferno_plus_TimeCodes()
+    test_inferno_plus_extraction()
+    
+# ----------------------------------------------------------------------------------------------------
+def test_inferno_plus_extraction():
+    print("--- test_inferno_plus_extraction")
+
+    elanXmlFilename = "../testData/test_alignable/inferno-threeLines_plus_one.eaf"
+    audioFilename = "../testData/test_alignable/inferno-threeLines.wav"
+    targetDirectory = "../testData/test_alignable/audio"
+    projectDirectory = "../testData/test_alignable"
+    tierGuideFile = "../testData/test_alignable/tierGuideInferno.yaml"
+    grammaticalTermsFile = "../testData/test_alignable/grammaticalTerms.txt"
+    fileList = os.listdir(targetDirectory)
+    for f in fileList:
+        target = os.path.join(targetDirectory,f)
+        os.remove(target)
+    # ae = AudioExtractor(audioFilename, elanXmlFilename, targetDirectory)
+    # ae.determineStartAndEndTimes()
+    # ae.extract()
+    # fileList = [f for f in os.listdir(targetDirectory) if not f.startswith('.')]
+    # try:
+    #     assert(len(fileList) == 3)
+    # except AssertionError as e:
+    #     print("Error: There are %d audiophrases rather than 3" %len(fileList))
+        # raise Exception(len(fileList)) from e
+    text = Text(elanXmlFilename,
+                audioFilename,
+                grammaticalTermsFile=grammaticalTermsFile,
+                tierGuideFile=tierGuideFile,
+                projectDirectory=projectDirectory)
+
+    # ae = AudioExtractor(audioFilename, elanXmlFilename, targetDirectory)
+    # ae.determineStartAndEndTimes()
+    # ae.extract()
+
+    htmlText = text.toHTML()
+
+    fileList = [f for f in os.listdir(targetDirectory) if not f.startswith('.')]
+    try:
+        assert(len(fileList) == 3)
+    except AssertionError as e:
+        print("Error: There are %d audiophrases rather than 3" %len(fileList))
+        raise Exception(len(fileList)) from e
+
+    display = False
+    if (display):
+        filename = "../testData/test_alignable/inferno-threeLines_plus.html"
+        f = open(filename, "w")
+        f.write(indent(htmlText))
+        f.close()
+        os.system("open %s" % filename)
+
+# ----------------------------------------------------------------------------------------------------
+def test_inferno_plus_TimeCodes():
+    print("--- test_inferno_plus_TimeCodes")
+
+    filename = "../testData/test_alignable/inferno-threeLines_plus_one.eaf"
+    doc = etree.parse(filename)
+    tierGuideFile = "../testTextPyData/inferno/tierGuide.yaml"
+    with open(tierGuideFile, 'r') as f:
+        tierGuide = yaml.safe_load(f)
+
+    x3 = IjalLine(doc, 1, tierGuide)
+    x3.parse()
+    tbl = x3.getTable()
+    startTime = x3.getStartTime()
+    endTime = x3.getEndTime()
+    print(startTime,endTime)
+
+# ----------------------------------------------------------------------------------------------------
+def test_HMDL_TimeCodes():
+    print("--- test_HMDL_TimeCodes")
+
+    filename = "../testData/HMDLsafe/HMDL.eaf"
+    doc = etree.parse(filename)
+    tierGuideFile = "../testData/HMDLsafe/tierGuide.yaml"
+    with open(tierGuideFile, 'r') as f:
+        tierGuide = yaml.safe_load(f)
+
+    x3 = IjalLine(doc, 1, tierGuide)
+    x3.parse()
+    tbl = x3.getTable()
+    startTime = x3.getStartTime()
+    endTime = x3.getEndTime()
+    print(startTime,endTime)
+
+# ----------------------------------------------------------------------------------------------------
+def test_aktzini_TimeCodes():
+    print("--- test_aktzini_TimeCodes")
+
+    filename = "../testData/aktzini/18-06-03Aktzini-GA.eaf"
+    doc = etree.parse(filename)
+    tierGuideFile = "../testData/aktzini/tierGuide.yaml"
+    with open(tierGuideFile, 'r') as f:
+        tierGuide = yaml.safe_load(f)
+
+    x3 = IjalLine(doc, 1, tierGuide)
+    x3.parse()
+    tbl = x3.getTable()
+    startTime = x3.getStartTime()
+    endTime = x3.getEndTime()
+    print(startTime,endTime)
+
+# ----------------------------------------------------------------------------------------------------
+def test_Ghost_TimeCodes():
+    print("--- test_Ghost_TimeCodes")
+
+    filename = "../testTextPyData/GhostInWagon/GhostInWagon.eaf"
+    doc = etree.parse(filename)
+    tierGuideFile = "../testTextPyData/GhostInWagon/tierGuide.yaml"
+    with open(tierGuideFile, 'r') as f:
+        tierGuide = yaml.safe_load(f)
+
+    x3 = IjalLine(doc, 1, tierGuide)
+    x3.parse()
+    tbl = x3.getTable()
+    startTime = x3.getStartTime()
+    endTime = x3.getEndTime()
+    print(startTime,endTime)
+
+# ----------------------------------------------------------------------------------------------------
+def test_inferno_TimeCodes():
+    print("--- test_inferno_TimeCodes")
+
+    filename = "../testTextPyData/inferno/inferno-threeLines.eaf"
+    doc = etree.parse(filename)
+    tierGuideFile = "../testTextPyData/inferno/tierGuide.yaml"
+    with open(tierGuideFile, 'r') as f:
+        tierGuide = yaml.safe_load(f)
+
+    x3 = IjalLine(doc, 1, tierGuide)
+    x3.parse()
+    tbl = x3.getTable()
+    startTime = x3.getStartTime()
+    endTime = x3.getEndTime()
+    print(startTime,endTime)
+
+# ----------------------------------------------------------------------------------------------------
+def test_Jagpossum_TimeCodes():
+    print("--- test_Jagpossum_TimeCodes")
+
+    filename = "../testTextPyData/Jagpossum/Jagpossum.eaf"
+    doc = etree.parse(filename)
+    tierGuideFile = "../testTextPyData/Jagpossum/tierGuide.yaml"
+    with open(tierGuideFile, 'r') as f:
+        tierGuide = yaml.safe_load(f)
+
+    x3 = IjalLine(doc, 1, tierGuide)
+    x3.parse()
+    tbl = x3.getTable()
+    startTime = x3.getStartTime()
+    endTime = x3.getEndTime()
+    print(startTime,endTime)
+    # assert (startTime == 8850.0)
+    # assert (endTime == 10570.0)
 
 # ----------------------------------------------------------------------------------------------------
 def test_Aktzini_plus_one(display):
@@ -40,6 +201,7 @@ def test_Aktzini_plus_one(display):
                 projectDirectory=projectDirectory)
 
     htmlText = text.toHTML()
+
     if (display):
         filename = "../testData/test_alignable/Aktzini_plus_one.html"
         f = open(filename, "w")
